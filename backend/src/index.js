7e1333b4f8be8d2c3640767f0d8d7672891db358
faiyaz-10 <faiyazshahriar.cse2023@gmail.com>
2026-07-25 13:06:27 +0600

    another commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import { clerkMiddleware } from '@clerk/express'
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+
 const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware())
 
-app.listen(PORT, () => console.log("Server is running on port: " , PORT));
+app.get("/health", (req, res) => {
+    res.status(200).json({ ok: true });
+});
+
+connectDB();
+app.listen(PORT, () => console.log("Server is running on port: ", PORT));
