e4506699adc3a461c3eb3be4983fa1815ac1a2c4
faiyaz-10 <faiyazshahriar.cse2023@gmail.com>
2026-07-26 06:47:03 +0600

    message routes and controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,7 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 import clerkWebhook from "./webhooks/clerk.webhook.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 const app = express();
 
@@ -27,6 +28,7 @@ app.get("/health", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // if the public directory exists, serve the static files
 // this is for the production build
